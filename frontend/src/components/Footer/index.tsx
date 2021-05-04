5fe25594480c101830db6717dba9eed6f4c095be
Rafael Grando <sta_rafinha@hotmail.com>
2021-05-04 04:30:09 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return (
+
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/" target="_blank" rel="noreferrer">Rafael Grando</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+            Evento promovido pela escola DevSuperior: <a href="https://www.linkedin.com/in/rafael-grando-691790137/" target="_blank" rel="noreferrer">@RafikiCWB</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
