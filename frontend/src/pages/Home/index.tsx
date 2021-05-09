caefad8ebb5f8a076f3b99fe32e048ca16971b20
Rafael Grando <sta_rafinha@hotmail.com>
2021-05-09 11:49:05 -0300

    Home, navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,9 +1,22 @@
+import NavBar from "components/NavBar";
+import { Link } from "react-router-dom";
 
 const Home = () => {
     return (
-        <div>
-            <h1>Página Home</h1>
-        </div>
+        <>
+            <NavBar />
+            <div className="container">
+                <div className="jumbotron">
+                    <h1 className="display-5">DSVendas</h1>
+                    <p className="lead"> Analise o desempenho das suas vendas por diferentes érséctovas</p>
+                    <hr />
+                    <p>Está aplicação consiste em exibir um dashboard a partir de dados fornecidos por um backend construído com Spring Boot</p>
+                    <Link className="btn btn-primary btn-lg" to="/dashboard">
+                    </Link>
+                </div>
+            </div>
+            <footer />
+        </>
     );
 }
 
